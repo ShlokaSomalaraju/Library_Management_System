8363355c90c971c30f9f3adf13134391bac84eed
Rishitha745 <132282780+Rishitha745@users.noreply.github.com>
2023-07-13 20:53:37 +0530

    modified:   Library/index.php

--- Library/index.php
+++ Library/index.php
@@ -1,5 +1,60 @@
 <!DOCTYPE html>
 <html>
+<?php
+$connect = new mysqli('localhost', 'root', '', 'Library');
+if (!$connect) {
+  die(mysqli_error($connect));
+}
+if (isset($_POST['submit'])) {
+  $user_email = $_POST['email'];
+  $user_password = $_POST['password'];
+  $check_email = "SELECT * FROM `Users` WHERE user_email='$user_email'";
+  $check_email_result = mysqli_query($connect, $check_email);
+  $number_email = mysqli_num_rows($check_email_result);
+  if ($number_email > 0) {
+    $check_password = "SELECT * FROM `Users` WHERE user_email='$user_email' AND user_password='$user_password'";
+    $check_password_result = mysqli_query($connect, $check_password);
+    $number_password = mysqli_num_rows($check_password_result);
+    if ($number_password > 0) {
+      $check_admin = "SELECT * FROM `Users` WHERE user_email='$user_email' AND user_password='$user_password'AND role='admin'";
+      $check_admin_result = mysqli_query($connect, $check_admin);
+      $number_admin = mysqli_num_rows($check_admin_result);
+      if ($number_admin > 0) {
+        session_start();
+        $_SESSION['admin_email'] = $user_email;
+        $_SESSION['is_logged'] = true;
+        echo
+        "<script>
+                    alert('Logged In successfully,Redirect to admin page');
+                    window.location.href='./adminhome.php';       
+                </script>";
+      } else {
+        session_start();
+        $_SESSION['user_email'] = $user_email;
+        $_SESSION['is_logged'] = true;
+        echo
+        "<script>
+                    alert('Successfully Logged In');
+                    window.location.href='./userhome.php';    
+                </script>";
+      }
+    } else {
+      echo
+      "<script>
+                alert('Incorrect Password');     
+            </script>";
+    }
+  } else {
+    echo
+    "<script>
+                alert('User not Registered');
+                window.location.href='./signup_page.php';       
+            </script>";
+  }
+}
+
+
+?>
 
 <head>
   <title>Our login Web page</title>
@@ -13,7 +68,7 @@
     <div class="back-image">
       <div class="box">
         <div class="login">
-          <form action="../all_HTML/signup.html" method="POST" enctype="multipart/form-data">
+          <form action="" method="POST" enctype="multipart/form-data">
             <fieldset class="filed">
 
               <legend class="loginbox">Login</legend>
@@ -39,7 +94,7 @@
             </fieldset>
           </form>
           <div class="para">
-            <p>Don't have an account?&nbsp;<a href="../all_HTML/signup.html" class="signup">Signup</a></p>
+            <p>Don't have an account?&nbsp;<a href="signup_page.php" class="signup">Signup</a></p>
           </div>
         </div>
 
